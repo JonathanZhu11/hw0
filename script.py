ee941d1359e2681449c648d9bb1a3ba87ee3c745
Ubuntu <azureuser@jonathanzhu.jonathanzhu.b7.internal.cloudapp.net>
2015-09-10 16:06:33 +0000

    Added script for single malt scotch

---
+++ script.py
@@ -0,0 +1,8 @@
+f = open('iowa-liquor-sample.csv', 'r')
+categories = f.readline().split(",")
+count = 0
+for line in f:
+	for word in line.split(","):
+		if word.lower() == "single malt scotch":
+			count += 1
+print count
